16bfd7dc20c4fa45c1780c2e61402bb129887655
Lelton Borges <lelton.borges@fornecedores.sicoob.com.br>
2023-01-14 15:18:37 -0300

    chore: variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let idade:i32 = 31;
+    println!("valor = {}, tamanho = {}", idade, std::mem::size_of_val(&idade));
+
+    let decimal  = 2.5;
+    println!("Decimal = {}, tamanho = {}", decimal, std::mem::size_of_val(&decimal));
+
+    let boleana =  false;
+    println!("Boleana = {}, tamanho = {}", boleana, std::mem::size_of_val(&boleana));
+
+    let letra = 'C';
+    println!("Char = {}, tamanho = {}", letra, std::mem::size_of_val(&letra));
+
 }
