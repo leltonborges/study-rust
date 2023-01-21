8fe0ed9651667b6ee07bfd8e429bb84342d4de37
Lelton Borges <lelton.borges@fornecedores.sicoob.com.br>
2023-01-21 17:38:53 -0300

    feat: struct

--- common/src/main.rs
+++ common/src/main.rs
@@ -3,17 +3,20 @@ mod matrix;
 mod colors;
 mod option;
 mod vertors;
+mod structs;
 
 use colors::{color, color::Color};
 use matrix::matriz;
 use week_days::week_and_days;
 use option::conteudo;
 use vertors::vect;
+use structs::example_accounts;
 
 fn main() {
     // arrays();
     // matrix();
     // week_and_days();
     // conteudo();
-    vect();
+    // vect();
+    example_accounts();
 }
